c414ba7b8a8e35fa576ffc74c7d9f2a7e1afbdf3
hamil <hamil@hamil-PC>
2018-05-04 21:05:17 -0400

    Primer commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Historia Dominicana</title>
+        <link rel="stylesheet" type="text/css" href="css/Style.css">
+        <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+        <section class="principal">
+            <div class="letras">
+                <article class="marginCenter maxWidth letras">
+                    <header>
+                        <h1 class="center">Historia de la Republica Dominicana</h1>
+                    </header>
+                        <div class="desarrolloArticulo" style="overflow: hidden !important">
+                            <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresion bajo los Haitianos. 
+                            Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
+                            Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
+                            militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
+                            varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
+                            independencia dominicana.</p>
+                        </div>
+                </article>
+            </div>
+        </section>
+        <section class="segundo">
+            <article>
+                <div class="center horizontal-center"><h1>Historia de la República Dominicana</h1></div>
+            </article>
+        </section>
+        
+        <script>
+            $(document).on('div.center','click',function(){alert('Jquery está funcionando.')});
+        </script>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
