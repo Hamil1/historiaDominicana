470a9b967708fb44649477ff8af7a57a1d9fbb27
hamil <hamil@hamil-PC>
2018-05-19 14:46:50 -0400

    + efectos visuales

--- index.php
+++ index.php
@@ -8,12 +8,11 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>Historia Dominicana</title>
+        <!--Css-->
         <link rel="stylesheet" type="text/css" href="css/Style.css">
         <link rel="stylesheet" type="text/css" href="css/normalize.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
         
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
         <section class="principal">
@@ -43,17 +42,46 @@ and open the template in the editor.
             </div>
         </section>
         <br>
-        <section class="segundo">
-            <article>
-                <div class="center horizontal-center"><h1>Historia de la República Dominicana</h1></div>
-            </article>
+        <section class="segundo" style="margin-top: 300px">
+            <div class="letras">
+                <article class="marginCenter maxWidth letras">
+                        <h2 class="center bold">Historia de la Republica Dominicana</h2>
+                        <div class="desarrolloArticulo" style="overflow: hidden !important">
+                            <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos. 
+                            Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
+                            Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
+                            militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
+                            varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
+                            independencia dominicana.</p>
+                            <div>
+                                <img src="./imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" class="padresdelapatria">
+                            </div>
+                            <p>
+                                La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por 
+                                Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
+                            </p>
+                            <p>
+                                Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle 
+                                al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
+                            </p>
+                        </div>
+                </article>
+            </div>
         </section>
-        
-        <script>
-            $(document).on('div.center','click',function(){alert('Jquery está funcionando.')});
-        </script>
         <?php
         // put your code here
         ?>
+        <!--Todos los documentos JavaScript-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="./js/Script.js" type="text/javascript"></script>
+        <script src="./js/TweenMax.js" type="text/javascript"></script>
+        <script src="./js/ScrollToPlugin.js" type="text/javascript"></script>
+        <script src="./js/EasePack.js" type="text/javascript"></script>
+        <script src="./js/EaselPlugin.js" type="text/javascript"></script>
+        <script src="./js/animate-scroll.js" type="text/javascript"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+        
     </body>
 </html>
---
+++ js/Script.js
@@ -0,0 +1,35 @@
+/* 
+ Project: HistoriaDominicana
+ File: Script 
+ Created by Hamil on May 5, 2018 - 10:13:27 AM. 
+ */
+
+
+$(document).ready(function(){
+    console.log("El documento 'Script' de Historia Dominicana está listo.");
+    
+    var titulo = $('article h2'),
+    parrafo = $('div.desarrolloArticulo'),
+    efecto = new TimelineLite();
+    
+    efecto.from(titulo, 0.3, {x: -500, autoAlpha: 0, ease:Power1.easeOut, delay: 0.2})
+            .from(parrafo, 0.3, {y: -50, autoAlpha: 0, ease:Power1.easeOut, delay: 0.2});
+    
+//    var controller = new ScrollMagic.Controller(); //Controlador de Scroll Magic
+//    $('article').each(function(){
+//        var tween = TweenMax.from($(this), 0.6,{y: -15, autoAlpha: 0, ease:Power1.easeOut, delay:0.7});
+//        
+//        var scene = new ScrollMagic.Scene({
+//            triggerElement: this
+//        })
+//                .setTween(tween)
+//                .addTo(controller);
+//    });
+//    var scene = new ScrollMagic.Scene({ //Haciendo la escena
+//      triggerElement: 'article'
+//    })
+//    .setClassToggle('article','show')
+//    .addTo(controller);
+
+
+});
