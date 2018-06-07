5d8c24b986546e23be106f21ecb6936f4cfa98e4
hamil <hamil@hamil-PC>
2018-06-06 21:07:41 -0400

    + funcion en javascript para replicar las escenas (efectos visuales) en varios triggers

--- index.php
+++ index.php
@@ -15,70 +15,97 @@ and open the template in the editor.
         
     </head>
     <body>
-        <section class="principal">
-            <div class="letras">
-                <article class="marginCenter maxWidth letras magic">
-                    <h2 class="center bold ">Historia de la Republica Dominicana</h2>
-                        <div class="desarrolloArticulo " style="overflow: hidden !important">
-                            <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos. 
-                            Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
-                            Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
-                            militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
-                            varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
-                            independencia dominicana.</p>
-                            <div>
-                                <img src="./imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" class="padresdelapatria">
-                            </div>
-                            <p>
-                                La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por 
-                                Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
-                            </p>
-                            <p>
-                                Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle 
-                                al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
-                            </p>
+        <div class="letras">
+            <article class="marginCenter maxWidth letras first">
+                <h2 class="center bold ">Historia de la Republica Dominicana</h2>
+                    <div class="desarrolloArticulo " style="overflow: hidden !important">
+                        <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos. 
+                        Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
+                        Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
+                        militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
+                        varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
+                        independencia dominicana.</p>
+                        <div>
+                            <img src="./imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" class="padresdelapatria">
                         </div>
-                </article>
-            </div>
-        </section>
+
+                        <div class="magic"></div>
+                        <p>
+                            La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por 
+                            Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
+                        </p>
+                        <p>
+                            Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle 
+                            al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
+                        </p>
+                    </div>
+            </article>
+        </div>
+        <div class="letras">
+            <article class="marginCenter maxWidth letras">
+                    <h2 class="center bold">Historia de la Republica Dominicana</h2>
+                    <div class="desarrolloArticulo" style="overflow: hidden !important">
+                        <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos. 
+                                Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
+                        Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
+                        militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
+                        varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
+                        independencia dominicana.</p>
+                        <div>
+                            <img src="./imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" class="padresdelapatria">
+                        </div>
+                        <div class="magic"></div>
+                        <p>
+                            La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por 
+                            Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
+                        </p>
+                        <p>
+                            Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle 
+                            al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
+                        </p>
+                    </div>
+            </article>
+        </div>
+        <div class="letras">
+            <article class="marginCenter maxWidth letras">
+                    <h2 class="center bold">Historia de la Republica Dominicana</h2>
+                    <div class="desarrolloArticulo" style="overflow: hidden !important">
+                        <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos. 
+                                Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
+                        Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
+                        militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
+                        varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
+                        independencia dominicana.</p>
+                        <div>
+                            <img src="./imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" class="padresdelapatria">
+                        </div>
+                        <div class="magic"></div>
+                        <p>
+                            La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por 
+                            Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
+                        </p>
+                        <p>
+                            Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle 
+                            al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
+                        </p>
+                    </div>
+            </article>
+        </div>
         <br>
-        <section class="segundo">
-            <div class="letras">
-                <article class="marginCenter maxWidth letras magic">
-                        <h2 class="center bold">Historia de la Republica Dominicana</h2>
-                        <div class="desarrolloArticulo" style="overflow: hidden !important">
-                            <p class="center letras">La República Dominicana se fundó en el año 1844 después de muchos años de opresión bajo los Haitianos. 
-                                    Durante los 22 años que precedieron a la independencia, toda la isla de La Española estuvo bajo el dominio de Haití. 
-                            Después de los esfuerzos hechos por patriotas dominicanos para independizar el país del dominio haitiano, varias acciones
-                            militares ocurridas entre 1844 y 1856 terminaron por consolidar a la república como un nuevo estado. Los haitianos intentaron
-                            varias veces volver a dominar la recién creada república con resultados fallidos hasta que en 1867 Haití reconoció la 
-                            independencia dominicana.</p>
-                            <div>
-                                <img src="./imagenes/padresDeLaPatria.jpeg" alt="Padres de la patria" class="padresdelapatria">
-                            </div>
-                            <p>
-                                La palabra Dominicana proviene del latín Dominicus, que significa Domingo. Sin embargo, la isla tiene este nombre por 
-                                Santo Domingo de Guzmán, fundador de la Orden de los Dominicos.
-                            </p>
-                            <p>
-                                Esta es una introducción a todo lo que usted será capaz de ver en esta página web, para seguir viendo más puede darle 
-                                al menú de la parte superior izquierda y elegir un evento que usted desee (estos eventos están ordenados de forma cronologica).
-                            </p>
-                        </div>
-                </article>
-            </div>
-        </section>
+        
         <?php
         // put your code here
         ?>
         <!--Todos los documentos JavaScript-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src="./js/Script.js" type="text/javascript"></script>
-        <script src="./js/TweenMax.js" type="text/javascript"></script>
-        <script src="./js/animate-scroll.js" type="text/javascript"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.0/TweenMax.min.js"></script>
+        <script src="./js/Scrollmagic/ScrollMagic.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/jquery.ScrollMagic.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
         <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
-        
+        <script src="./js/velocity.min.js" type="text/javascript"></script>
+        <script src="./js/Scrollmagic/plugins/animation.velocity.js" type="text/javascript"></script>
+        <script src="./js/Script.js" type="text/javascript"></script>
     </body>
 </html>
--- js/Script.js
+++ js/Script.js
@@ -8,26 +8,15 @@
 $(document).ready(function(){
     console.log("El documento 'Script' de Historia Dominicana está listo.");
     
-    var titulo = $('article.magic h2'),
-    parrafo = $('article.magic div.desarrolloArticulo'),
-    efecto = new TimelineLite();
-    
-    efecto.from(titulo, 0.5, {x: -300, autoAlpha: 0, ease: Power1.easeOut, delay: 0.3})
-            .from(parrafo, 0.3, {y: -50, autoAlpha: 0, ease:Power1.easeOut, delay: 0.2});
-    
     var controller = new ScrollMagic.Controller(); //Controlador de Scroll Magic
-
-    var scene = new ScrollMagic.Scene({ //Haciendo la escena
-      triggerElement: '.magic',
-      triggerHook: 0
-    })
-    .setClassToggle('.magic','show')
-    .addIndicators({
-        name: 'fade scene',
-        colorTrigger: 'black',
-        colorStart: '#75C695'
-    })
-    .addTo(controller);
-
-
+    
+    $('div.magic').each(function(){
+        var scene = new ScrollMagic.Scene({ //Haciendo la escena
+            triggerElement: this,
+            triggerHook: 0
+        }).setVelocity($(this).closest('article'),{opacity: 0}, {duration: 400})//Libreria de animación
+        .addIndicators()
+        .addTo(controller);
+    });
+    
 });
