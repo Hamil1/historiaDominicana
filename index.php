520e4030d2480705a8986d724d303401485047b1
Víctor Díaz <victor.diaz@Optic.local>
2018-07-04 14:08:33 -0400

    tipo de letra

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor......
         <link rel="stylesheet" type="text/css" href="css/modalLogin.css">
         <link rel="stylesheet" type="text/css" href="css/normalize.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
