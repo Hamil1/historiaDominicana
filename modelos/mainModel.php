ecd5e243bd20d695e85ec36f18f0db177df6ccfa
Hamil <hamil151@hotmail.com>
2018-07-15 21:27:35 -0400

    - apellido

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -8,10 +8,9 @@ class mainModel{
         R::setup('mysql:host=localhost;dbname=historiaDominicana', 'root','');
     }
 
-    public function crearUsuario($nombre, $apellido, $correo, $contrasena, $fecha_creacion = false){
+    public function crearUsuario($nombre, $correo, $contrasena, $fecha_creacion = false){
         $usuario = R::dispense('usuarios');
         $usuario->nombre = $nombre;
-        $usuario->apellido = $apellido;
         $usuario->correo = $correo;
         $usuario->contrasena = sha1($contrasena);
         $usuario->fecha_creacion = (!$fecha_creacion)?time():$fecha_creacion;
