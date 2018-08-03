2cd00bf26eb8655ae33c863a18e622028a6c69d6
Víctor Díaz <victor.diaz@Optic.local>
2018-08-03 16:55:14 -0400

    guarda usuario

--- controladores/mainController.php
+++ controladores/mainController.php
@@ -9,6 +9,7 @@
                 switch ($_POST['metodo']){
                     case "agregarArticulo": $this->agregarArticulo(); break;
                     case "iniciarSesion": $this->iniciarSesion(); break;
+                    case "crearUsuario": $this->crearUsuario(); break;
                 }
             }
         }
@@ -23,7 +24,12 @@
         }
 
         public function crearUsuario(){
-            
+            $modelo = new mainModel();
+            $nombre = $_POST['nombre'];
+            $correo = $_POST['correo'];
+            $contrasena = $_POST['contrasena'];
+            $fechaCreacion = time();
+            $modelo->crearUsuario($nombre, $correo, $contrasena, $fechaCreacion);
         }
 
     }
